410d56b0bb2fc7e50f69ec5bb71a767c4364d8b6
Douyer <zyl09281219@163.com>
2019-03-07 21:52:33 +0800

    CSS分离  CSS背景图片URL处理

--- config/webpack.dev.js
+++ config/webpack.dev.js
@@ -1,8 +1,13 @@
-
-
-
 const path = require('path');
+//JS压缩
+const uglify = require('uglifyjs-webpack-plugin');
+//html打包
 const htmlPlugins = require('html-webpack-plugin');
+//CSS分离
+const extractCssPlugin = require('mini-css-extract-plugin'); 
+//实例化CSS分离对象
+// const extractCSS = new extractCssPlugin("css/[name].css");
+var website = {publicPath: 'http://localhost:8090'};
 module.exports = {
     //开发环境
     mode: "development",
@@ -15,7 +20,8 @@ module.exports = {
         //打包后的文件路径
         path: path.resolve(__dirname,"../dist"),
         //打包后的文件名称，[name]表示打包之后的文件名仍然使用打包之前的文件名
-        filename: '[name].js'
+        filename: '[name].js',
+        publicPath: website.publicPath
     },
     //模块
     module:{
@@ -24,23 +30,44 @@ module.exports = {
             {
                 test: /\.css$/,
                 use: [
-                    {loader: "style-loader"},
-                    {loader: "css-loader"}
-                   
+                    "style-loader",
+                    extractCssPlugin.loader,
+                    "css-loader"
                 ]
-    
+                    
+                // [
+                //     {loader: "style-loader"},
+                //     {loader: "css-loader"}
+                // ]
+            },
+            //CSS样式中的图片加载
+            {
+                test:/\.(png|jpg|gif|jpeg)/,
+                use: [
+                    {
+                        loader: 'url-loader',
+                        options: {
+                            limit: 500,
+                        }
+                    }
+                ]
             }
         ]
     },
     //插件
     plugins:[
+        //压缩JS
+        new uglify(),
+        //html打包
         new htmlPlugins({
             minify:{ //对html进行压缩
                 removeAttributeQuotes:true 
             },
             hash: true,   //JS缓存
             template: "./src/index.html"   //将要打包的html文件
-        })
+        }),
+        //css从JS中分离开来
+        new extractCssPlugin("css/[name].css")
     ],
     //配置webpack开发服务
     devServer:{
--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,6 @@
 
 import css from '../css/index.css';
+import css01 from '../css/main.css';
 
 
 console.info('222222');
