b8a24f4789ad18ffc3a625e60874131468dcc3d6
Douyer <zyl09281219@163.com>
2019-03-08 15:59:40 +0800

    完成html打包 JS压缩打包 CSS打包，css中图片路径处理  html文件中图片路径的处理

--- config/webpack.dev.js
+++ config/webpack.dev.js
@@ -1,13 +1,13 @@
+//webpack4.29.6
 const path = require('path');
 //JS压缩
 const uglify = require('uglifyjs-webpack-plugin');
 //html打包
 const htmlPlugins = require('html-webpack-plugin');
-//CSS分离
+//CSS分离  webpack 4.0版本必须使用min-css-extract-plugin插件，用extract-text-plugin插件来打包会报错
 const extractCssPlugin = require('mini-css-extract-plugin'); 
-//实例化CSS分离对象
-// const extractCSS = new extractCssPlugin("css/[name].css");
-var website = {publicPath: 'http://localhost:8090'};
+//定义对象来处理静态文件路径
+var website = {publicPath: '/'};
 module.exports = {
     //开发环境
     mode: "development",
@@ -17,10 +17,12 @@ module.exports = {
     },
     //出口文件配置
     output:{
-        //打包后的文件路径
+        //打包后的文件根目录路径
         path: path.resolve(__dirname,"../dist"),
-        //打包后的文件名称，[name]表示打包之后的文件名仍然使用打包之前的文件名
-        filename: '[name].js',
+        //打包后的JS文件路径，[name]表示打包之后的文件名仍然使用打包之前的文件名
+        //JS打包后的位置：dist目录下的js文件夹下面的name.js
+        filename: './js/[name].js',
+        //处理静态文件路径,以dist作为根目录，以绝对路径来处理
         publicPath: website.publicPath
     },
     //模块
@@ -47,10 +49,18 @@ module.exports = {
                     {
                         loader: 'url-loader',
                         options: {
+                            //小于500B的文件会打包成base64的格式写入JS中
                             limit: 500,
+                            //img图片打包之后的路径：dist/images/
+                            outputPath: './images/'
                         }
                     }
                 ]
+            },
+            //html中图片加载
+            {
+                test: /\.(html|htm)$/i,
+                use: ["html-withimg-loader"]
             }
         ]
     },
@@ -60,14 +70,15 @@ module.exports = {
         new uglify(),
         //html打包
         new htmlPlugins({
-            minify:{ //对html进行压缩
-                removeAttributeQuotes:true 
-            },
             hash: true,   //JS缓存
             template: "./src/index.html"   //将要打包的html文件
         }),
         //css从JS中分离开来
-        new extractCssPlugin("css/[name].css")
+        new extractCssPlugin(
+            {
+                filename:"./css/[name].css"
+            }
+        )
     ],
     //配置webpack开发服务
     devServer:{
